c2ec6925a6a90fa295250d338689c8626d045775
PaulGyan <gyanpaulkwasi@gmail.com>
2024-08-17 14:55:38 +0000

    Added new files

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from . import views
+from .views import list_books
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('libraries/<pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<pk>/', views.delete_book, name='delete_book'),
 ]
