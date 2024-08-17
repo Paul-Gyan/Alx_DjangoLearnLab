abaa0f590adf0a636a4c5df96840df0735b79333
PaulGyan <gyanpaulkwasi@gmail.com>
2024-08-17 15:24:53 +0000

    Added new files

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -25,5 +28,32 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-        
+
+class userprofile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=['Admin', 'Librarian', 'Memeber'])
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        userprofile.objects.create(user=instance)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication = models.CharField(max_length=100)
+
+    class Meta:
+        Permission = (
+            ('is Admin', 'Is Admin'),
+            ('can_add_book', 'Can Add Book'),
+            ('can_delete_book', 'Can Delete Book'),
+            ('can_change_book', 'Can Change Book'),
+        )
+
+
+
+
 
