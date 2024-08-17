5d94079d3ce2232c067b59964e180ae62b5fad4f
PaulGyan <gyanpaulkwasi@gmail.com>
2024-08-17 17:26:18 +0000

    Added new files

---
+++ django-models/relationship_app/member_view.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+def member_view(request):
+    return render(request, 'member_view.html')
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
