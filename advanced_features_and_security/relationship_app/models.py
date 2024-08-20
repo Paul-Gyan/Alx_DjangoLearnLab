4e43685b4d15c8e42ab5d23b39fcd74801bdec02
PaulGyan <gyanpaulkwasi@gmail.com>
2024-08-20 16:10:06 +0000

    Added new file

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -1,79 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
-
-class UserManager(models.Manager):
-    def create_user(self, username, email, password, date_of_birth, profile_photo):
-        if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(
-            username=username,
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-            profile_photo=profile_photo,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password, date_of_birth, profile_photo):
-        user = self.create_user(
-            username,
-            email,
-            password,
-            date_of_birth,
-            profile_photo,
-        )
-        user.is_admin = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-    
-    
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
-
-
 class Book(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField()
 
     def __str__(self):
         return self.title
-
 class Library(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
         return self.name
-
 class Librarian(models.Model):
-    name = models.CharField(max_length=255)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-class UserProfile(models.Model):
+class userprofile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=['Admin', 'Librarian', 'Member'])
+    role = models.CharField(max_length=50, choices=['Admin', 'Librarian', 'Memeber'])
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        userprofile.objects.create(user=instance)
 
 
 class Book(models.Model):
@@ -83,8 +47,16 @@ class Book(models.Model):
 
     class Meta:
         permissions = (
-            ('is_admin', 'Is Admin'),
-            ('can_add_book', 'Can add book'),
-            ('can_change_book', 'Can change book'),
-            ('can_delete_book', 'Can delete book'),
+            ('is admin', 'Is Admin'),
+            ('can_add_book', 'Can Add Book'),
+            ('can_delete_book', 'Can Delete Book'),
+            ('can_change_book', 'Can Change Book'),
         )
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=[('Admin', 'Admin'), ('Librarian', 'Librarian'), ('Member', 'Member')])
+
+
+
+
+
